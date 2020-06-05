ee132dbe6d051d6e64d9f71a29c126c49a835fea
Georg Schmid <georg.schmid@epfl.ch>
2020-06-05 18:19:44 +0200

    More parsing

--- src/l3/ast.rs
+++ src/l3/ast.rs
@@ -1,7 +1,24 @@
-#![allow(non_snake_case)]
+#![allow(non_snake_case, dead_code)]
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+use std::hash::{Hash, Hasher};
+use std::collections::HashMap;
+pub use std::rc::Rc;
+
+#[derive(Debug, Copy, Clone)]
 pub struct Name<'a>(pub &'a str);
+
+impl<'a> PartialEq for Name<'a> {
+  fn eq(&self, other: &Self) -> bool {
+    *self.0 == *other.0
+  }
+}
+impl<'a> Eq for Name<'a> {}
+
+impl<'a> Hash for Name<'a> {
+  fn hash<H: Hasher>(&self, state: &mut H) {
+    (*self.0).hash(state);
+  }
+}
 
 type BlockTag = u8;
 
@@ -42,12 +59,94 @@ pub enum Atom<'a> {
 
 pub type Arg<'a>  = Atom<'a>;
 pub type Args<'a> = Vec<Arg<'a>>;
+pub type Params<'a> = Vec<Name<'a>>;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Cnt<'a> {
+  pub name: Name<'a>,
+  pub args: Params<'a>,
+  pub body: Box<Tree<'a>>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Fun<'a> {
+  pub name: Name<'a>,
+  pub retC: Name<'a>,
+  pub args: Params<'a>,
+  pub body: Box<Tree<'a>>,
+}
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum Tree<'a> {
   LetP { name: Name<'a>, prim: ValuePrimitive, args: Args<'a>, body: Box<Tree<'a>> },
+  LetC { cnts: Vec<Rc<Cnt<'a>>>, body: Box<Tree<'a>> },
+  LetF { funs: Vec<Rc<Fun<'a>>>, body: Box<Tree<'a>> },
   AppC { cnt: Name<'a>, args: Args<'a> },
   AppF { fun: Arg<'a>, retC: Name<'a>, args: Args<'a> },
   If { cond: TestPrimitive, args: Args<'a>, thenC: Name<'a>, elseC: Name<'a> },
   Halt { arg: Atom<'a> },
+}
+
+#[derive(Debug)]
+pub struct Program<'a> {
+  tree: Box<Tree<'a>>,
+  symbols: Symbols<'a>,
+}
+
+#[derive(Debug)]
+pub struct Symbols<'a> {
+  cnts: HashMap<Name<'a>, Rc<Cnt<'a>>>,
+  funs: HashMap<Name<'a>, Rc<Fun<'a>>>,
+}
+
+impl<'a> Symbols<'a> {
+  fn register_cnt(&mut self, cnt: Rc<Cnt<'a>>) -> () {
+    assert!(!self.cnts.contains_key(&cnt.name));
+    self.cnts.insert(cnt.name, cnt);
+  }
+
+  fn register_fun(&mut self, fun: Rc<Fun<'a>>) -> () {
+    assert!(!self.funs.contains_key(&fun.name));
+    self.funs.insert(fun.name, fun);
+  }
+}
+
+impl<'a> Program<'a> {
+  fn new(tree: Box<Tree<'a>>) -> Self {
+    Self {
+      tree: tree,
+      symbols: Symbols {
+        cnts: HashMap::new(),
+        funs: HashMap::new()
+      }
+    }
+  }
+
+  pub fn build_from_tree(tree: Box<Tree<'a>>) -> Self {
+    fn traverse<'a>(symbols: &mut Symbols<'a>, tree: &Tree<'a>) -> () {
+      use Tree::*;
+      match &tree {
+        LetP { body, .. } => traverse(symbols, body),
+        LetC { cnts, body } => {
+          for cnt in cnts {
+            symbols.register_cnt(cnt.clone());
+            traverse(symbols, &cnt.body)
+          }
+          traverse(symbols, body)
+        },
+        LetF { funs, body } => {
+          for fun in funs {
+            symbols.register_fun(fun.clone());
+            traverse(symbols, &fun.body)
+          }
+          traverse(symbols, body)
+        },
+        _ => ()
+      }
+    }
+
+    let mut program = Self::new(tree);
+    traverse(&mut program.symbols, &program.tree);
+    program
+  }
 }
--- src/l3/parser.rs
+++ src/l3/parser.rs
@@ -9,19 +9,23 @@ use pest::error::Error;
 #[grammar = "l3/l3-cps.pest"]
 pub struct L3CPSParser;
 
-fn parse_l3cps(input: &str) -> Result<Box<Tree>, Error<Rule>> {
-    use pest::iterators::Pair;
+fn parse_l3cps_program(input: &str) -> Result<Program, Error<Rule>> {
+    use pest::iterators::{Pair, Pairs};
 
     fn parse_name(pair: Pair<Rule>) -> Name {
       Name(pair.as_str())
     }
 
-    fn parse_arg(pair: Pair<Rule>) -> Atom {
+    fn parse_arg(pair: Pair<Rule>) -> Arg {
       match pair.as_rule() {
         Rule::name => Atom::AtomN(parse_name(pair)),
         Rule::literal => Atom::AtomL(pair.as_str().parse().unwrap()),
         _ => unreachable!(),
       }
+    }
+
+    fn parse_args(pairs: Pairs<Rule>) -> Args {
+      pairs.map(|pair| parse_arg(pair)).collect()
     }
 
     fn parse_test_prim(pair: Pair<Rule>) -> TestPrimitive {
@@ -66,31 +70,79 @@ fn parse_l3cps(input: &str) -> Result<Box<Tree>, Error<Rule>> {
       }
     }
 
-    // fn parse_binding(pair: Pair<Rule>, body: Box<Tree>) -> Box<Tree> {
-    //   let mut pairs = pair.into_inner();
-    //   let name = parise_name(pairs.next().unwrap());
-    //   let
-    // }
+    // Update body if it is a LetC, otherwise wrap in new LetC
+    fn push_cnt<'a>(name: Name<'a>, args: Params<'a>, cnt_body: Box<Tree<'a>>, body: Box<Tree<'a>>) -> Box<Tree<'a>> {
+      let mut body = body;
+      let cnt = Rc::new(Cnt { name: name, args: args, body: cnt_body });
+      match body.as_mut() {
+        Tree::LetC { cnts, .. } => {
+          cnts.push(cnt);
+          body
+        },
+        _ => Box::new(Tree::LetC { cnts: vec![cnt], body: body })
+      }
+    }
+
+    // Update body if it is a LetF, otherwise wrap in new LetF
+    fn push_fun<'a>(name: Name<'a>, ret_cnt: Name<'a>, args: Params<'a>, fun_body: Box<Tree<'a>>, body: Box<Tree<'a>>) -> Box<Tree<'a>> {
+      let mut body = body;
+      let fun = Rc::new(Fun { name: name, retC: ret_cnt, args: args, body: fun_body });
+      match body.as_mut() {
+        Tree::LetF { funs, .. } => {
+          funs.push(fun);
+          body
+        },
+        _ => Box::new(Tree::LetF { funs: vec![fun], body: body })
+      }
+    }
+
+    fn parse_binding<'a>(pair: Pair<'a, Rule>, body: Box<Tree<'a>>) -> Box<Tree<'a>> {
+      let mut pairs = pair.into_inner();
+      let name = parse_name(pairs.next().unwrap());
+      let rhs_pair = pairs.next().unwrap();
+
+      match rhs_pair.as_rule() {
+        Rule::prim_bdg => {
+          let mut pairs = rhs_pair.into_inner();
+          let prim = parse_value_prim(pairs.next().unwrap());
+          let args = parse_args(pairs);
+          Box::new(Tree::LetP { name: name, prim: prim, args: args, body: body })
+        },
+        Rule::cnt_bdg => {
+          let mut pairs = rhs_pair.into_inner();
+          let args = pairs.next().unwrap().into_inner().map(|pair| parse_name(pair)).collect();
+          let cnt_body = parse_tree(pairs.next().unwrap());
+          push_cnt(name, args, cnt_body, body)
+        },
+        Rule::fun_bdg => {
+          let mut pairs = rhs_pair.into_inner();
+          let ret_cnt = parse_name(pairs.next().unwrap());
+          let args = pairs.next().unwrap().into_inner().map(|pair| parse_name(pair)).collect();
+          let fun_body = parse_tree(pairs.next().unwrap());
+          push_fun(name, ret_cnt, args, fun_body, body)
+        },
+        _ => unreachable!()
+      }
+    }
 
     fn parse_tree(pair: Pair<Rule>) -> Box<Tree> {
-      Box::new(match pair.as_rule() {
+      match pair.as_rule() {
         Rule::lett => {
           let mut pairs = pair.into_inner();
-          let bindings_pairs = pairs.next().unwrap().into_inner().rev();
+          let bindings_pairs = pairs.next().unwrap().into_inner();
           let body = parse_tree(pairs.next().unwrap());
-          println!("XXX body: {:#?}\nYYY bindings: {:#?}", body, bindings_pairs);
-          unreachable!()
+          bindings_pairs.rfold(body, |acc, pair| parse_binding(pair, acc))
         },
         Rule::app => {
           let mut pairs = pair.into_inner();
           let name = parse_name(pairs.next().unwrap());
-          let args: Args = pairs.map(|pair| parse_arg(pair)).collect();
+          let args = parse_args(pairs);
           // TODO: Implement this stuff properly
-          if (args.is_empty()) {
+          Box::new(if args.is_empty() {
             Tree::AppC { cnt: name, args: vec![] }
           } else {
             Tree::AppF { fun: Atom::AtomN(name.clone()), retC: name, args: args }
-          }
+          })
         },
         Rule::iff => {
           let mut pairs = pair.into_inner();
@@ -99,18 +151,21 @@ fn parse_l3cps(input: &str) -> Result<Box<Tree>, Error<Rule>> {
           let arg2 = parse_arg(pairs.next().unwrap());
           let cnt1 = parse_name(pairs.next().unwrap());
           let cnt2 = parse_name(pairs.next().unwrap());
-          Tree::If { cond: cond, args: vec![arg1, arg2], thenC: cnt1, elseC: cnt2 }
+          Box::new(Tree::If { cond: cond, args: vec![arg1, arg2], thenC: cnt1, elseC: cnt2 })
         },
-        Rule::halt => Tree::Halt { arg: parse_arg(pair.into_inner().next().unwrap()) },
+        Rule::halt =>
+          Box::new(Tree::Halt { arg: parse_arg(pair.into_inner().next().unwrap()) }),
         Rule::tree
         | Rule::EOI
         | Rule::WHITESPACE => unreachable!(),
         ru => { println!("Rule: {:?}", ru); panic!("UH") }
-      })
+      }
     }
 
-    let tree = L3CPSParser::parse(Rule::tree, input)?.next().unwrap();
-    Ok(parse_tree(tree))
+    let parsed = L3CPSParser::parse(Rule::tree, input)?.next().unwrap();
+    let tree = parse_tree(parsed);
+    let program = Program::build_from_tree(tree);
+    Ok(program)
 }
 
 
@@ -118,58 +173,62 @@ macro_rules! parse_test {
   ($name:ident, $example:expr) => {
     #[test]
     fn $name() {
-      println!("RES: {:?}", parse_l3cps($example)
-        .unwrap_or_else(|e| { panic!("Parsing error: {}", e) }));
+      let _res = parse_l3cps_program($example)
+        .unwrap_or_else(|e| { panic!("Parsing error: {}", e) });
+      // println!("RES: {:#?}", res);
     }
   }
 }
 
-// parse_test!(test_halt, "(halt 0)");
-// parse_test!(test_halt_withnl, "(halt\nb)");
-// parse_test!(test_app_f0, "(f 0)");
-// parse_test!(test_app_fn, "(f n)");
-// parse_test!(test_app_fabc, "(f a b 123 c)");
-// parse_test!(test_app_fwithnums, "(f999 r88 g7)");
-// parse_test!(test_if_lt, "(if (@< 0 x) c1 c2)");
-// parse_test!(test_if_le, "(if (@<= x y) c1 c2)");
-// parse_test!(test_if_eq, "(if (@= x y) c1 c2)");
+parse_test!(test_halt, "(halt 0)");
+parse_test!(test_halt_withnl, "(halt\nb)");
+parse_test!(test_app_f0, "(f 0)");
+parse_test!(test_app_fn, "(f n)");
+parse_test!(test_app_fabc, "(f a b 123 c)");
+parse_test!(test_app_fwithnums, "(f999 r88 g7)");
+parse_test!(test_if_lt, "(if (@< 0 x) c1 c2)");
+parse_test!(test_if_le, "(if (@<= x y) c1 c2)");
+parse_test!(test_if_eq, "(if (@= x y) c1 c2)");
 parse_test!(test_let_one_prim,    "(let ((a (@id 0))) (halt a))");
-// parse_test!(test_let_many_prims,  "(let* ((a (@id 0)) (b (@+ 1 2))) (halt b))");
-// parse_test!(test_let_cnt,         "(let* ((c (cnt () (halt 0)))) (c))");
-// parse_test!(test_let_fun,         "(let* ((f (fun (r x) (r x)))) (halt b))");
-// parse_test!(test_fact_example, "
-// (let* ((fact_1
-//   (fun (ret-fact x)
-//     (let* ((then_1 (cnt () (ret-fact 3)))
-//            (else_1
-//             (cnt ()
-//               (let* ((v_9 (@- x 2))
-//                      (rc_1
-//                       (cnt (r_2)
-//                         (let* ((x_2 (@xor x 1))
-//                                (r_3 (@shift-right r_2 1))
-//                                (x_1 (@* x_2 r_3))
-//                                (v_10 (@xor x_1 1)))
-//                           (ret-fact v_10)))))
-//                 (fact_1 rc_1 v_9)))))
-//       (if (@= 1 x) then_1 else_1))))
-//  (t_1 (@byte-read))
-//  (t (@shift-left t_1 1))
-//  (v (@xor t 1))
-//  (then (cnt () (let ((v_8 (@byte-write -54))) (halt 0))))
-//  (else
-//   (cnt ()
-//     (let* ((v_1 (@- v 2))
-//            (rc
-//             (cnt (r)
-//               (let* ((v_6 (@xor v 1))
-//                      (r_1 (@shift-right r 1))
-//                      (v_5 (@* v_6 r_1))
-//                      (v_4 (@xor v_5 1))
-//                      (v_3 (@- v_4 110))
-//                      (v_2 (@shift-right v_3 1))
-//                      (v_7 (@byte-write v_2)))
-//                 (halt 0)))))
-//       (fact_1 rc v_1)))))
-// (if (@= 1 v) then else))
-// ");
+parse_test!(test_let_many_prims,  "(let* ((a (@id 0)) (b (@+ 1 2))) (halt b))");
+parse_test!(test_let_cnt1,        "(let* ((c (cnt () (halt 0)))) (c))");
+parse_test!(test_let_cnt2,        "(let* ((c1 (cnt () (c2 0))) (c2 (cnt (x) (halt x)))) (c1))");
+parse_test!(test_let_fun1,        "(let* ((f (fun (r x) (r x)))) (halt 0))");
+parse_test!(test_let_fun2,        "(let* ((f (fun (r x) (r x))) (g (fun (r x) (f r x)))) (halt 0))");
+// parse_test!(test_let_fun_bad,     "(let* ((f (fun (r x) (r x))) (f (fun (r x) (f r x)))) (halt 0))");
+parse_test!(test_fact_example, "\
+(let* ((fact_1
+  (fun (ret-fact x)
+    (let* ((then_1 (cnt () (ret-fact 3)))
+           (else_1
+            (cnt ()
+              (let* ((v_9 (@- x 2))
+                     (rc_1
+                      (cnt (r_2)
+                        (let* ((x_2 (@xor x 1))
+                               (r_3 (@shift-right r_2 1))
+                               (x_1 (@* x_2 r_3))
+                               (v_10 (@xor x_1 1)))
+                          (ret-fact v_10)))))
+                (fact_1 rc_1 v_9)))))
+      (if (@= 1 x) then_1 else_1))))
+ (t_1 (@byte-read))
+ (t (@shift-left t_1 1))
+ (v (@xor t 1))
+ (then (cnt () (let ((v_8 (@byte-write -54))) (halt 0))))
+ (else
+  (cnt ()
+    (let* ((v_1 (@- v 2))
+           (rc
+            (cnt (r)
+              (let* ((v_6 (@xor v 1))
+                     (r_1 (@shift-right r 1))
+                     (v_5 (@* v_6 r_1))
+                     (v_4 (@xor v_5 1))
+                     (v_3 (@- v_4 110))
+                     (v_2 (@shift-right v_3 1))
+                     (v_7 (@byte-write v_2)))
+                (halt 0)))))
+      (fact_1 rc v_1)))))
+(if (@= 1 v) then else))
+");
